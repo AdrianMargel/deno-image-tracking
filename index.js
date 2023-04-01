1d05ff93b5fbd1f1a2ed78fd2b53ee95c4b6ec5c
AdrianMargel <carbongrouplife@gmail.com>
2023-03-31 20:15:28 -0600

    log to mongodb

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import {
+  Bson,
+  MongoClient,
+} from "https://deno.land/x/mongo@LATEST_VERSION/mod.ts";
+
+const dbClient=new MongoClient();
+// Connect to Mongo Atlas Database
+await client.connect({
+  db: "image-tracking",
+  tls: true,
+  servers: [
+    {
+      host: "main-cluster.tyflron.mongodb.net",
+      port: 27017,
+    },
+  ],
+  credential: {
+    username: "adrian",
+    password: Deno.env.get("MONGO_PASSWORD"),//yeah, not a chance I was going to put that here directly
+    db: "image-tracking",
+    mechanism: "SCRAM-SHA-1",
+  },
+});
+const db=client.database("image-tracking");
+const tracks=db.collection("tracks");
 
 const logoImg=await Deno.readFile('./logo.png');
 
 const handler=(request)=>{
 	const ua=request.headers.get("user-agent")??"Unknown";
+	const url=request.url;
 
 	const head=new Headers();
 	head.set('content-type','image/png');
 
-	let img=logoImg;
+	const img=logoImg;
+	const insertId=await tracks.insertOne({
+		ua,
+		url,
+	});
 
 	return new Response(img, {
 		headers: head,
