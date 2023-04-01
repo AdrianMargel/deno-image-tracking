fbedb1eda0ba9959a07edadf7ac673171cd7570d
AdrianMargel <carbongrouplife@gmail.com>
2023-03-31 19:01:25 -0600

    serve image

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
-const handler = (request) => {
-	const body = `Your user-agent is:\n\n${
-		request.headers.get("user-agent") ?? "Unknown"
-	}`;
+const logoImg=await Deno.readFile('./logo.png');
 
-	return new Response(body, { status: 200 });
+const handler=(request)=>{
+	const ua=request.headers.get("user-agent")??"Unknown";
+
+	const head=new Headers();
+	head.set('content-type','image/png');
+
+	let img=logoImg;
+
+	return new Response(img, {
+		headers: head,
+		status: 200
+	});
 };
 
 serve(handler);
